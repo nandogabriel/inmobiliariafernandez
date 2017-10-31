aaaf6843b3003cf58c0f6145d7158ac0273ea02e
Fernando <nandogabriel1987@gmail.com>
2017-10-31 18:29:53 -0300

    agregamos index

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<h1>
+	<center>hola mundo la fecha de hoy es<center>
+	<?php
+		echo "Today is " . date("d/m/Y H:i:s") . "<br>";
+		echo "Today is " . date("Y.m.d") . "<br>";
+		echo "Today is " . date("Y-m-d") . "<br>";
+		echo "Today is " . date("l");
+	?>
+	</center>
+</h1>
+<center>
+	<img src="imagenes/casa 1.jpg">
+<center>
+
+<h1>h1. Bootstrap heading <small>Secondary text</small></h1>
+<h2>h2. Bootstrap heading <small>Secondary text</small></h2>
+<h3>h3. Bootstrap heading <small>Secondary text</small></h3>
+<h4>h4. Bootstrap heading <small>Secondary text</small></h4>
+<h5>h5. Bootstrap heading <small>Secondary text</small></h5>
+<h6>h6. Bootstrap heading <small>Secondary text</small></h6>
